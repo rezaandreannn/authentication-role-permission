6e46db280cc734227116d256d8fdff15a68d136e
rezaandreannn <andreanreza042@gmail.com>
2024-12-10 10:35:20 +0700

    add lang profile

---
+++ lang/en/profile.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Profile',
+    'information' => 'Profile Information',
+    'sub-information' => "Update your account's profile information and email address.",
+    'emailunverified' => "Your email address is unverified.",
+    'btn-send-verify-email' => "Click here to re-send the verification email.",
+    'verification-link-sent' => 'A new verification link has been sent to your email address.',
+    'btn-saved' => "Save",
+    'update-password' => 'Update password',
+    'sub-update-password' => 'Ensure your account is using a long, random password to stay secure.',
+    'current-password' => 'Current Password',
+    'new-password' => 'New Password',
+    'confirm-password' => 'Confirm Password',
+
+
+];
---
+++ lang/id/profile.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Profil',
+    'information' => 'Informasi Profil',
+    'sub-information' => "Perbarui informasi profil dan alamat email akun Anda.",
+    'email-unverified' => "Alamat email Anda belum diverifikasi.",
+    'btn-send-verify-email' => "Klik di sini untuk mengirim ulang email verifikasi.",
+    'verification-link-sent' => 'Tautan verifikasi baru telah dikirim ke alamat email Anda.',
+    'btn-saved' => "Simpan",
+    'update-password' => 'Perbarui kata sandi',
+    'sub-update-password' => 'Pastikan akun Anda menggunakan kata sandi acak yang panjang agar tetap aman.',
+    'current-password' => 'Kata Sandi Saat Ini',
+    'new-password' => 'Kata Sandi Baru',
+    'confirm-password' => 'Konfirmasi Kata Sandi',
+    'delete-account' => 'Hapus Akun',
+    'sub-delete-account' => 'Setelah akun Anda dihapus, semua sumber daya dan datanya akan dihapus secara permanen. Sebelum menghapus akun Anda, silakan unduh data atau informasi apa pun yang ingin Anda simpan.'
+];
